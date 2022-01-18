4064b03a96ad6ece1d3b0e9310e61316b3e58c95
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-01-18 19:44:43 -0300

    Nav functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 
+/* ------------------- Toggle Navbar -------------------- */
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+}
+
 /* ------------------- About Tabs -------------------- */
 const tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
