fcafee0e96ee1858b7e9af9074d6d894dd3204e9
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2022-01-18 20:13:28 -0300

    Navigation between sections

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ function toggleNavbar(){
     document.querySelector(".header").classList.toggle("active");
 }
 
+/* ------------------- Active Section -------------------- */
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+        }, 500);
+    }
+})
+
 /* ------------------- About Tabs -------------------- */
 const tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
